9b3b4e91068d7feebb0c9982dc562c8f31199e4e
Dragos Stefanescu <dragos.stefanescu@ing.ro>
2018-09-11 12:54:01 +0200

    api with marten

---
+++ Api/Controllers/TodosController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Marten;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Api.Controllers
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        [Required] public string Title { get; set; }
+        public bool Completed { get; set; } = false;
+    }
+
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        IDocumentStore store ;
+        public TodosController(IDocumentStore store) => this.store = store;
+        
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Todo>> Get()
+        {
+            int count;
+            List<Todo> todos;
+
+            using (var sess = store.OpenSession())
+            {
+                count = sess.Query<Todo>().Count();
+                var ucount = sess.Query<User>().Count();
+            }
+
+            if (count == 0)
+            {
+                using (var session = store.LightweightSession())
+                {
+                    session.Store(new Todo {Title = "first one"});
+                    session.Store(new Todo {Title = "second one"});
+                    session.SaveChanges();
+                }
+            }
+
+            using (var sess = store.LightweightSession())
+            {
+                todos = sess.Query<Todo>().ToList();
+            }
+
+            return Ok(todos);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Todo> Get(string id)
+        {
+            using (var sess = store.LightweightSession())
+            {
+                return Ok( sess.Load<Todo>(Guid.Parse(id)));
+            }
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<Todo> Post([FromBody] Todo todo)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            using (var sess = store.OpenSession())
+            {
+                sess.Store(todo);
+                sess.SaveChanges();
+   
+                return Created(nameof(Get), todo);
+            }
+
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using HerokuPGParser;
+using Marten;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            // Marten document store
+            var conStr = "host=localhost;database=martendb;password=;username=;port=1000";
+            var pgConn = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            if (!string.IsNullOrWhiteSpace(pgConn))
+                conStr = HerokuPGParser.ConnectionHelper.BuildExpectedConnectionString(pgConn);
+            
+            services.AddScoped<IDocumentStore>(provider => 
+                DocumentStore.For(conStr));
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Swagger def", Version = "v1" });
+            });
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+          //  app.UseHttpsRedirection();
+            app.UseMvc();
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todos API"); });
+
+        }
+    }
+}
